d70b4519b78e6006730c3bc927d75767128d7d8c
SEBASTIEN NAZAIRE <nazaire@nike.cs.uga.edu>
2018-04-23 18:21:30 -0400

    First commit

--- ftclient.py
+++ ftclient.py
@@ -7,6 +7,7 @@ import platform
 import os
 import binascii
 import struct
+import textwrap
 
 mode = None
 
@@ -95,60 +96,63 @@ class ft(Thread):
 
 def init_to_server(mode, address, ID):
 	
-	# Split the HOST:PORT string
-	addr_port = address.split(':')
-	
-	SERVER_HOST = addr_port[0]
-	SERVER_PORT = int(addr_port[1])
-	SERVER_ADDR = (SERVER_HOST, SERVER_PORT)
-
-	### Sending and recieving byte strings
-	connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	connection.connect(SERVER_ADDR)
-	print("Asking \'" + addr_port[0] + ":" + addr_port[1] + " about an ID...")
-
-	if mode == 0:
-		connection.send(b'0')
-		print("Issued ID for identification...")
-		recv_addr = None
-	else:
-		connection.send(b'1')
-		connection.send(b''+ ID.encode())
-		recv_addr = connection.recv(256)
-
-
-	connection.close()
-	return recv_addr
+  # Split the HOST:PORT string
+  addr_port = address.split(':')
+  
+  SERVER_HOST = addr_port[0]
+  SERVER_PORT = int(addr_port[1])
+  SERVER_ADDR = (SERVER_HOST, SERVER_PORT)
+  
+  ### Sending and recieving byte strings
+  connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  connection.connect(SERVER_ADDR)
+  print("Asking \'" + addr_port[0] + ":" + addr_port[1] + " about an ID...")
+  
+  if mode == 0:
+    connection.send(b'0')
+    print("Issued ID for identification...")
+    recv_addr = None
+  else:
+    connection.send(b'1')
+    connection.send(b''+ ID.encode())
+    recv_addr = connection.recv(256)
+  
+  connection.close()
+  return recv_addr
 
 def run_client(receiver, filename, cons, size):
 	
 	# Split the HOST:PORT string
-	addr_port = address.split(':')
+  addr_port = address.split(':')
 
-	connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	recv_client = (receiver[0], int(receiver[1]))
-	connection.connect(recv_client)
+  connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  recv_client = (receiver[0], int(receiver[1]))
+  connection.connect(recv_client)
 
-	sendFile = open(filename, "rb")
-	temp = sendFile.read()
+  sendFile = open(filename, "rb")
+  temp = sendFile.read()
+  CNUM = 4
+  offset = (len(temp))/CNUM
+  tempsplit = textwrap.wrap(temp, offset)
+  print(tempsplit)
+  start = 0;
+  
+ 
+  print("Sending \'" + filename + "\'...")
+  header_string = b''
+  header_string += filename.encode()
+  header_string += b'\x00'
+  start  = offset * i
+  header_string += struct.pack("I", len(temp))
+  header_string = header_string + temp
+  
 
-
-
-	print("Sending \'" + filename + "\'...")
-
-	header_string = b''
-	header_string += filename.encode()
-	header_string += b'\x00'
-	header_string += struct.pack("I", len(temp))
-	header_string = header_string + temp
-
-	
-	totalsent = 0
-	while totalsent < len(header_string):
-	    sent = connection.send(header_string[totalsent:])
-	    if sent == 0:
-	        raise RuntimeError("socket connection broken")
-	    totalsent = totalsent + sent
+  totalsent = 0
+  while totalsent < len(header_string):
+      sent = connection.send(header_string[totalsent:])
+      if sent == 0:
+          raise RuntimeError("socket connection broken")
+      totalsent = totalsent + sent
 
 
 def run_recv(port, size):
@@ -181,13 +185,13 @@ parser.add_argument('--receive', action='store_true')
 parser.add_argument('--send', nargs = 2)
 
 # Indicates what the client's buffer size should be. (Default 4096) 
-parser.add_argument('-s', '--size', default=4096, action='store')
+parser.add_argument('-s', '--size', default=4096, action='store', type=int)
 
 # Indicates the port that this receiver client will use for it's socket
-parser.add_argument('-p','--port', action='store')
+parser.add_argument('-p','--port', action='store', type=int, default=47695)
 
 # Specifies the number of parallel, concurrent connections to use when sending (default 1)
-parser.add_argument('-c', '--cons', default=1, action='store')
+parser.add_argument('-c', '--cons', default=1, action='store', type=int)
 args = parser.parse_args()
 
 address = args.server
@@ -196,10 +200,11 @@ address = args.server
 if args.receive:
 	mode = 0
 	init_to_server(mode, address, None)
-	run_recv(args.p, args.s)
+	run_recv(args.port, args.size)
 else:
-	mode = 1
-	recv_addr = init_to_server(mode, address, args.send[0])
-	receiver = recv_addr.decode("utf-8").split(',')
-	print("Found client at \'" + receiver[0] + ":" + receiver[1] + "\'...")
-	run_client(receiver, args.send[1], args.c, args.s)
+  mode = 1
+  recv_addr = init_to_server(mode, address, args.send[0])
+  receiver = recv_addr.decode("utf-8").split(',')
+  print(receiver)
+  print("Found client at \'" + receiver[0] + ":" + receiver[1] + "\'...")
+  run_client(receiver, args.send[1], args.cons, args.size)
